de0f78dfc3c5f86515e982f3bc17460b4a542f23
davidingles <davidingleslopez@gmail.com>
2023-07-18 13:20:01 +0200

    prueba y más pruebas

---
+++ src/data/menu.ts
@@ -0,0 +1,19 @@
+import { Menu } from "../types/types"
+export const menu:Menu[] = [
+    {
+        titulo: "Servicio",
+        url: "/servicios"
+    },
+    {
+        titulo: "Productos",
+        url: "/productos"
+    },
+    {
+        titulo: "Catálogo",
+        url: "/catalogo"
+    },
+    {
+        titulo: "Sobre nosotros",
+        url: "/us"
+    },
+]
